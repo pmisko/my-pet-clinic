777459d1dbeaf0b4c18cc94a4b68952b7723a159
pmisko <miskopiotr@gmail.com>
2020-10-18 15:26:48 +0200

    Display list od owners on owners/index page. Closes #16

--- pet-clinic-web/src/main/java/pl/pmisko/mypetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/pl/pmisko/mypetclinic/controllers/OwnersController.java
@@ -1,15 +1,27 @@
 package pl.pmisko.mypetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.pmisko.mypetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnersController {
 
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOfOwners(){
+    public String listOfOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
+
+
 }
