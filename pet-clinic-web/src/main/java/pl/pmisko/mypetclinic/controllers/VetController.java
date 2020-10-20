6c0691416ec98407103686273462b4c46fc99add
pmisko <miskopiotr@gmail.com>
2020-10-20 21:38:54 +0200

    Display list od vets on vets/index page. Closes #15

--- pet-clinic-web/src/main/java/pl/pmisko/mypetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pl/pmisko/mypetclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package pl.pmisko.mypetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.pmisko.mypetclinic.services.VetService;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "vets/index.html"})
-    public String listOfVets(){
+    public String listOfVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
