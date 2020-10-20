528e38746ae3792cc385b962e4522bfeb246aec7
pmisko <miskopiotr@gmail.com>
2020-10-20 22:02:13 +0200

    Update AbstractMap impl to manage objects id field. Closes #23

--- pet-clinic-web/src/main/java/pl/pmisko/mypetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/pl/pmisko/mypetclinic/bootstrap/DataLoader.java
@@ -23,14 +23,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Piotr");
         owner1.setLastName("Misko");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Piotrek");
         owner2.setLastName("Misko2");
 
@@ -39,14 +37,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Wet1");
         vet1.setLastName("Jeden");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("WET2");
         vet2.setLastName("Drugi");
 
