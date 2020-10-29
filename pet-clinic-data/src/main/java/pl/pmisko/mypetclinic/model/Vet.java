7aaf1ccc58369ef19d42c5d167c74636f2aceee5
pmisko <miskopiotr@gmail.com>
2020-10-29 17:48:59 +0100

    Add vet specialty . Closes #27

--- pet-clinic-data/src/main/java/pl/pmisko/mypetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/pl/pmisko/mypetclinic/model/Vet.java
@@ -1,4 +1,23 @@
 package pl.pmisko.mypetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    Set<Specialty> specialties;
+
+    public Vet() {
+    }
+
+    public Vet(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
