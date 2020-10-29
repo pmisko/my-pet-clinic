f5f952e0aae2f56f5bcb9068aee605b80fecc843
pmisko <miskopiotr@gmail.com>
2020-10-29 17:38:56 +0100

    Add Visit Entity. Closes #26

--- pet-clinic-data/src/main/java/pl/pmisko/mypetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/pl/pmisko/mypetclinic/model/Owner.java
@@ -1,4 +1,23 @@
 package pl.pmisko.mypetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person{
+
+    Set<Pet> pets;
+
+    public Owner() {
+    }
+
+    public Owner(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
