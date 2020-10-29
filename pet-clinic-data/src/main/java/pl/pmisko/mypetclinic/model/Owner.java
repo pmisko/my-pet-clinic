6b7d5510a29e14bc99d7ad5cfa0b46b01bceca24
pmisko <miskopiotr@gmail.com>
2020-10-29 17:53:20 +0100

    Add personal details to owner. Closes #28

--- pet-clinic-data/src/main/java/pl/pmisko/mypetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/pl/pmisko/mypetclinic/model/Owner.java
@@ -4,13 +4,33 @@ import java.util.Set;
 
 public class Owner extends Person{
 
+    private String address;
+    private String city;
+    private String telephone;
     Set<Pet> pets;
 
-    public Owner() {
+    public String getAddress() {
+        return address;
     }
 
-    public Owner(Set<Pet> pets) {
-        this.pets = pets;
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 
     public Set<Pet> getPets() {
