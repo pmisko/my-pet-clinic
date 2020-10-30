bbd7244981dc719a3cce6a3b14e09821c9fc9793
pmisko <miskopiotr@gmail.com>
2020-10-30 14:28:04 +0100

    Add Specialties Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/pl/pmisko/mypetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package pl.pmisko.mypetclinic.services;
+
+import pl.pmisko.mypetclinic.model.Specialty;
+
+public interface SpecialitiesService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/pl/pmisko/mypetclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,37 @@
+package pl.pmisko.mypetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import pl.pmisko.mypetclinic.model.Specialty;
+import pl.pmisko.mypetclinic.services.SpecialitiesService;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialitiesService {
+
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
